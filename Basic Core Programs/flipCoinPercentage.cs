d69b9851e960652fea539e0ff8123e0054fee02b
Amit <amitkr24031995@gmail.com>
2021-09-09 07:27:10 +0530

    Adding flipCoilPerPercentage

---
+++ Basic Core Programs/flipCoinPercentage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace flipCoinPercentage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //constants
+            int HEAD = 1;
+            Random random = new Random();
+            //Computation
+            int flip = random.Next(0, 2);
+            if (flip == HEAD)
+            {
+                Console.WriteLine("HEAD");
+            }
+            else
+            {
+                Console.WriteLine("Tail");
+            }
+        }
+    }
+}
